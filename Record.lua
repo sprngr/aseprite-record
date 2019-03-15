6acbbf291d5d3999cfc1bf2f6e32bdda20663b00
Michael Springer <mike@sprngr.com>
2019-03-14 22:38:59 -0400

    Minor progress on interface, worried about writing to file in background

--- Record.lua
+++ Record.lua
@@ -24,19 +24,58 @@ function split(s, delimiter)
     return result;
 end
 
-
 -- Check if a filename is set, otherwise skip these steps
-
-if sprite.filename then
-    -- Get file path separator, \ for Windows and / for everything else
-    local pathSeparator = sprite.filename:sub(1,1) == "/" and "/" or "\\"
-
-    -- Set up path 
-    local pathTable = split(sprite.filename, pathSeparator)
-    local baseFilename = pathTable[#pathTable]
-
-    table.remove(pathTable, #pathTable)
-
-    local workingDirectory = table.concat(pathTable,pathSeparator)
+if not sprite.filename then
+    app.alert("Please save the file to disk before starting Time Lapse.")
+    return
 end
- 
+
+-- Get file path separator, \ for Windows and / for everything else
+local pathSeparator = sprite.filename:sub(1,1) == "/" and "/" or "\\"
+
+-- Set up path
+local pathTable = split(sprite.filename, pathSeparator)
+local baseFilename = pathTable[#pathTable]
+
+table.remove(pathTable, #pathTable)
+local workingDirectory = table.concat(pathTable,pathSeparator)
+
+-- Create new Time Lapse filename
+function generateFilename()
+    return baseFilename:gsub('(%.%w+)$', '-time-lapse.aseprite')
+end
+
+function open(filename)
+    local saveFile = app.open(workingDirectory..pathSeparator..filename)
+
+    if not saveFile then
+        -- TODO Make new file
+    end
+end
+
+function write()
+    sprite.selection:selectAll()
+    app.command.CopyMerged()
+end
+
+-- Create dialog box
+local dlg = Dialog("Time Lapse v0.1")
+local bounds = dlg.bounds
+dlg.bounds = Rectangle(bounds.x, bounds.y, 256, 128)
+dlg:label{ id="originalFile",
+           label="Target File:",
+           text="./"..baseFilename }
+dlg:label{ id="timeLapseFile",
+           label="Save File:",
+           text="./"..generateFilename() }
+dlg:number{ id="timeLapseInterval",
+           text="60",
+           focus=true }
+dlg:separator{}
+dlg:button{
+    text = "Open Save File",
+		onclick = function() open(generateFilename()) end
+}
+
+
+dlg:show{ wait=false }
